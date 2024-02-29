16c938f5079e09d1fca4d03626422262ccb4039d
ZH-Jihan <zakirhossain19920@gmail.com>
2024-02-29 09:35:21 -0800

    chacke cradintial

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const employeeRoutes = require("./routes/v1/employee.routes");
 const hostelmemberRouts = require("./routes/v1/hostelmember.route");
 
 // Middleware
-app.use(cors(
-  {origin: 'http://localhost:3000', 
-  credentials: true,}
-));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
 
